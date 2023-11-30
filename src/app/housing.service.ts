37e616e5d42c0aa6879ea8b5f16e31308feec9d2
TherRipper482 <santiagogustavo882@gmail.com>
2023-11-30 11:34:01 -0600

    13 Mostar arrays

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -4,14 +4,22 @@ import {HousingLocation} from './housinglocation';
   providedIn: 'root',
 })
 export class HousingService {
+  readonly baseUrl = 'https://angular.dev/assets/tutorials/common';
+  
   url = 'http://localhost:3000/locations';
   async getAllHousingLocations(): Promise<HousingLocation[]> {
     const data = await fetch(this.url);
-    return (await data.json()) ?? [];
+    const houses: HousingLocation[] = (await data.json()) ?? [];
+    return houses.map((house:HousingLocation) => {
+      house.photo = this.baseUrl + house.photo;
+      return house;  
+    })
   }
   async getHousingLocationById(id: number): Promise<HousingLocation | undefined> {
     const data = await fetch(`${this.url}/${id}`);
-    return (await data.json()) ?? {};
+    const house:HousingLocation = (await data.json()) ?? {};
+    house.photo = this.baseUrl + house.photo;
+    return house;
   }
   submitApplication(firstName: string, lastName: string, email: string) {
     // tslint:disable-next-line
